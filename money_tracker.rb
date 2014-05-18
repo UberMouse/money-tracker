ac821a4f61d5df3ae2005bffc98c1879eff2ae53
= <=>
2014-05-18 23:09:53 +1200

    Changed homepage to display transactions saved to db

--- money_tracker.rb
+++ money_tracker.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
+require 'sequel'
+require 'haml'
+require 'hashie'
 require_relative 'bnz_importer/bnz_importer'
 
 class MoneyTracker < Sinatra::Application
   get '/' do
+    db = Sequel.sqlite('transactions.db')
+
+    transactions = db[:transactions].all.map{|t| Hashie::Mash.new t}
+
+    haml :index, :locals => {:transactions => transactions}
+  end
+
+  get '/sync' do
     BNZImporter.new('', '').import
   end
 end
